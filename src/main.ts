a6895fc41128d014934b27622a498f34bdc2158f
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-03-18 01:05:37 +0900

    fix: renderer process で node js api を実行させたくないため削除

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ function createWindow () {
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true,
             contextIsolation: false,
         }
     })
